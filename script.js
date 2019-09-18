19bd1cfd33cac52ab91ab208e609252deac28988
Rafael Mungnol <05220154@FARROUPILHA.IFRS.EDU.BR>
2019-09-17 21:14:40 -0300

    Criado lista

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.tabs').tabs();
 });
 
-fetch('http://www.mocky.io/v2/5d6ed7ab320000c0c1a8aabd', {
-    method: 'GET'
-  })
-  .then(function(response) {
-    response.json().then(function(data) {
-      // console.log(data);
-      mostra_dados(data);
-    });
-  })
-  .catch(function(err) {
-    console.error('Failed retrieving information', err);
-  });
-
-
-function mostra_dados(data) {
-  console.log(data);
-  // console.log("Aqui");
-  var $minhaTabela = $("#tabela_vendas_conteudo");
-  // console.log($minhaTabela);
-  var linha = "";
-  linha += "<tr><td>" + "Homens vs Mulheres" +"</td><td>" + "100 x 150" + "</td></tr>";
-
-  $minhaTabela.html(linha);
+function limparSelecao() {
+  var itens = $(".badge");//[0].innerHTML = 0
+  
+  for (const i in itens) {
+    itens[i].innerHTML = 0;
+  }
 }
 
-function limparSelecao() {
-
+function adiciona(e) {
+  var qtdeAntga = parseInt(e.firstChild.innerHTML);
+  e.firstChild.innerHTML = qtdeAntga + 1;
 }
